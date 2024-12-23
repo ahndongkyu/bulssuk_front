7762480cd0b5ef8cef883ce4b904b2482330b125
“dongkyu” <kjuvcz95@gamil.com>
2024-12-23 16:45:38 +0900

    main_connect

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
+      title: 'Seperate Recycling',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green, // 기본 테마 색상 (필요시 통합 조정)
       ),
+      debugShowCheckedModeBanner: false, // 디버그 배너 숨김
       initialRoute: '/login', // 앱 시작 화면을 로그인 페이지로 설정
       routes: {
         '/login': (context) => LoginPage(), // 로그인 화면 경로
